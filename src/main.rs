de5f6f689d0f8daac09ac98186b17fb823113bdc
Marco Toschi <marco.toschi@fastwebnet.it>
2023-02-13 00:50:49 +0100

    Few improvement

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use rand::Rng;
 use serde_derive::Deserialize;
-use std::error::Error;
 use std::fs;
-use std::process::exit;
 use toml;
 
 #[derive(Deserialize)]
@@ -28,39 +25,44 @@ struct Elements {
     adverbs: Vec<String>,
     objects: Vec<String>,
 }
+mod random_gen {
+    use rand::Rng;
+    use std::error::Error;
+    use std::process::exit;
 
-fn rand_element(e: &Vec<String>) -> &String {
-    let secret_number = rand::thread_rng().gen_range(0..=e.len() - 1);
-    &e[secret_number]
-}
+    pub fn rand_element(e: &Vec<String>) -> &String {
+        let secret_number = rand::thread_rng().gen_range(0..=e.len() - 1);
+        &e[secret_number]
+    }
 
-fn error_handler<F, T, E>(f: F) -> T
-where
-    F: Fn() -> Result<T, E>,
-    E: Error,
-{
-    match f() {
-        Ok(t) => t,
-        Err(e) => {
-            println!("An error occurred: {}", e);
-            exit(1);
+    pub fn error_handler<F, T, E>(f: F) -> T
+    where
+        F: Fn() -> Result<T, E>,
+        E: Error,
+    {
+        match f() {
+            Ok(t) => t,
+            Err(e) => {
+                println!("An error occurred: {}", e);
+                exit(1);
+            }
         }
     }
 }
-
 fn main() {
     // The path to the file to read
-    let path = "./data/stuff.toml";
+    let path = "./data/elements.toml";
 
     // Read the file contents into a string, returns `io::Result<usize>`
-    let raw_contents = error_handler(|| fs::read_to_string(path));
-    let contents: Content = error_handler(|| toml::from_str(&raw_contents));
 
-    // print!("{:?}", raw_contents);
+    let raw_contents = random_gen::error_handler(|| fs::read_to_string(path));
+
+    let contents: Content = random_gen::error_handler(|| toml::from_str(&raw_contents));
 
     let mut text = String::new();
+
     for el in contents.as_array() {
-        text.push_str(&rand_element(el));
+        text.push_str(&random_gen::rand_element(el));
         text.push_str(" ");
     }
 
